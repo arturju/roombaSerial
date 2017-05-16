4e1fd8d5624c1351a1c84afb606d51a30f497dab
Arturo Jumpa <arturju@hotmail.com>
2017-05-15 22:47:13 -0400

    started some machine state code for roomba control

--- roombaSerialBasic/inc/roomba.h
+++ roombaSerialBasic/inc/roomba.h
@@ -4,60 +4,10 @@
 /*-----------------------------------------------------------------------------
  ** Global Defines/Typedefs/Enums/Macros.
  **---------------------------------------------------------------------------*/
-#define clamp(value, min, max) (value < min ? min : value > max ? max : value)
-
-#define 	ON 			1
-#define 	OFF 		0
-#define 	MAX_SPEED 	220 // 70% of top speed ==> 256
-#define 	MIN_SPEED 	70 
-
-
-// variables for Roomba
 extern SoftwareSerial Roomba;
 
+
 int ddPin = 5; //device detect
-
-
-// int buttonPin = 12;
-// int ledPin=13;
-
-//variables for LEDs
-bool debrisLED;
-bool spotLED;
-bool dockLED;
-bool warningLED;
-byte color;
-byte intensity;
-
-char digit1;
-char digit2;
-char digit3;
-char digit4;
-
-//blocking sensor functions - these will request data and wait until a response is recieved, then return the response
-
-int getSensorData(byte sensorID);
-int * getSensorData(byte numOfRequests, byte requestIDs[]);
-
-bool getSensorData(byte * buffer, byte bufferLength);
-
-byte single_byte_packets[22] = { 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 21, 24, 34, 35, 36, 37, 38, 45, 52, 53, 58};
-bool is_in_array(byte val);
-
-
-
-/** LED controls --------------------
-- available in Safe or Full Mode
-
-
-*/
-
-/*---------------------------------------------------------------
-Serial sequence: [139] [LED Bits] [Power Color] [Power Intensity]
-LED Bits:		
-Power Color: 	 0 = green, 255 = red
-Power Intensity: 0= off, 	255 = full intensity
-*/
 
 
 /*-----------------------------------------------------------------------------
@@ -74,6 +24,25 @@ void wakeUp (void)
   delay(2000);
 }
 
+void cmdRoomba(int command){
+	/*
+	if(command != START_OI){
+		Roomba.write(START_OI);
+		delay(1000);
+		Roomba.write(command);
+	}else{
+		Roomba.write(START_OI);
+	}
+	*/
+	Roomba.write(command);
+	
+	
+	
+	delay(1000);
+	
+}
+
+
 
 /*-----------------------------------------------------------------------------
  ** Function: 
@@ -81,7 +50,7 @@ void wakeUp (void)
  **---------------------------------------------------------------------------*/
 void startSafe()
 {  
-  Roomba.write(128);  //Start
+  Roomba.write(128);  //Starts the OI, must always send before sending OI cmds
   Roomba.write(131);  //Safe mode
   delay(1000);
 }
@@ -159,105 +128,18 @@ void playSound (int num)
 
 
 
+/*--------------------------------- LED controls
+- Available in Safe or Full Mode
+- Serial sequence: [139] [LED Bits] [Power Color] [Power Intensity]
+- LED Bits:		
+- Power Color: 	 0 = green, 255 = red
+- Power Intensity: 0= off, 	255 = full intensity
+---------------------------------*/
 
 /*  
  This command controls the Power LED by two data bytes: color and intensity.
  color: Green = 0; 128 = Orange; red=255 
  intensity low=0; max=255 */
-void setPowerLED(byte setColor, byte setIntensity)
-{
-  color = setColor;
-  intensity = setIntensity;
-  Roomba.write(139);
-  Roomba.write((byte)0x00);
-  Roomba.write((byte)color);
-  Roomba.write((byte)intensity);
-}
-
-//---------------------------------------------------------------
-void setDebrisLED(bool enable)
-{
-  debrisLED = enable;
-  Roomba.write(139);
-  Roomba.write((debrisLED ? 1 : 0) + (spotLED ? 2 : 0) + (dockLED ? 4 : 0) + (warningLED ? 8 : 0));
-  Roomba.write((byte)color);
-  Roomba.write((byte)intensity);
-}
-
-//---------------------------------------------------------------
-void setSpotLED(bool enable)
-{
-  spotLED = enable;
-  Roomba.write(139);
-  Roomba.write((debrisLED ? 1 : 0) + (spotLED ? 2 : 0) + (dockLED ? 4 : 0) + (warningLED ? 8 : 0));
-  Roomba.write((byte)color);
-  Roomba.write((byte)intensity);
-}
-
-//---------------------------------------------------------------
-void setDockLED(bool enable)
-{
-  dockLED = enable;
-  Roomba.write(139);
-  Roomba.write((debrisLED ? 1 : 0) + (spotLED ? 2 : 0) + (dockLED ? 4 : 0) + (warningLED ? 8 : 0));
-  Roomba.write((byte)color);
-  Roomba.write((byte)intensity);
-}
-
-
-
-/*--------------------------------------------------------------------------
-This command controls the four 7 segment displays on the Roomba.*/
-void setDigitLEDs(byte digit1, byte digit2, byte digit3, byte digit4)
-{
-    Roomba.write(163);
-    Roomba.write(digit1);
-    Roomba.write(digit2);
-    Roomba.write(digit3);
-    Roomba.write(digit4);
-}
-
-/*--------------------------------------------------------------------------
-This command controls the four 7 segment displays on the Roomba using ASCII character codes.*/
-void setDigitLEDFromASCII(byte digit, char letter)
-{
-  switch (digit){
-  case 1:
-    digit1 = letter;
-    break;
-  case 2:
-    digit2 = letter;
-    break;
-  case 3:
-    digit3 = letter;
-    break;
-  case 4:
-    digit4 = letter;
-    break;
-  }
-  Roomba.write(164);
-  Roomba.write(digit1);
-  Roomba.write(digit2);
-  Roomba.write(digit3);
-  Roomba.write(digit4);
-}
-
-//---------------------------------------------------------------
-void cleanDigitLED (void)
-{
-  setDigitLEDFromASCII(1, ' ');
-  setDigitLEDFromASCII(2, ' ');
-  setDigitLEDFromASCII(3, ' ');
-  setDigitLEDFromASCII(4, ' ');
-}
-//---------------------------------------------------------------
-void writeLEDs (char a, char b, char c, char d)
-{
-  setDigitLEDFromASCII(1, a);
-  setDigitLEDFromASCII(2, b);
-  setDigitLEDFromASCII(3, c);
-  setDigitLEDFromASCII(4, d);
-}
 
 
 
--- roombaSerialBasic/roombaSerialBasic.ino
+++ roombaSerialBasic/roombaSerialBasic.ino
@@ -1,25 +1,28 @@
 /*------------------------------------------------------------------
-Basic control for Roomba via serial cmds
-
-
-
 Summary of Roomba Open Interface Spec:
 
 4 operating modes: Off, Passive, Safe, and Full
   -Passive(128): Sleeps after 5 min inactive. Other modes drain battery
   -Safe(131):    Allows user control but safety condition reverts it to Passive mode
   -Full(132):    Complete complete, ignores cliff, wheel-drop, charger, etc
-
 -------------------------------------------------------------------*/
-
 #include <SoftwareSerial.h>
+#define START_OI  128
+#define RESET_OI  7
+#define STOP_OI   173
+#define SAFE_MODE 131
+#define FULL_MODE 132
+#define CLEAN     135
+#define SEEK_DOCK 143
 
 #include "inc/roomba.h"
-
-//int rxPin=10;
-//int txPin=11;
 SoftwareSerial Roomba(10,11); //rxPin, txPin
 
+#define IDLING         1
+#define DOING_STUFF    2
+int roombaState = IDLING;
+int serialIn;
+  
 //---------------------------------------------
 void setup() 
 {
@@ -27,36 +30,39 @@ void setup()
   Serial.begin(19200);
   pinMode(ddPin, OUTPUT);  //pin5
 
-  delay(2000);
-  
-//  wakeUp ();  
-  startPassive();
-
-  // turn-off all LEDs and Display
-//  setPowerLED(128,0);
-//  setDebrisLED(OFF);
-//  setDockLED(OFF);
-//  setSpotLED(OFF);
-//  setWarningLED(OFF);
-//  cleanDigitLED ();
-
-  playSound (1); // start sound
-  Serial.println("Playing sound1");
-
-   delay(5000);
-   playSound (3);
-   Serial.println("Playing sound3");
-   
+  Serial.println("Setup done");
 }
 
 void loop() 
 {
-  int serialIn;
-  if (Serial.available()>0){
-    serialIn= Serial.read();
-    Serial.println(serialIn);    
-//    manualCmd();
-  }
+/* Add 86 from serial Input
+ *  clean:  135 -> 49. Chr 1
+ *  sDock:  143 -> 57. Chr 9
+ *  startOI:128 -> 42. Chr * 
+*/
+if (Serial.available()>0)    serialIn= Serial.read()+86;  
+
+if(serialIn != 0){
+  Serial.print("Cmd: "); Serial.println(serialIn);   
   
+  //If we're doing something, stop current cmd then issue new one
+  if (roombaState == DOING_STUFF){
+    cmdRoomba(CLEAN);   
+    delay(200);
+    cmdRoomba(serialIn);
+    roombaState = DOING_STUFF;
+  }else{
+    cmdRoomba(START_OI);  // must start OI again
+    delay(500);
+    cmdRoomba(serialIn);
+    roombaState = DOING_STUFF;
+  }
+
+  
+}// End if(serialIn)
+
+// timeOut roombaState to IDLE. After 5 min OI stops
+
+serialIn = 0; 
 }
 
